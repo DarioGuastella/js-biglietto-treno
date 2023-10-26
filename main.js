b876311e9a318e16d59606fda2ef3981dbc80fbc
DarioGuastella <jimmyguastella@gmail.com>
2023-10-26 17:33:37 +0200

    ticket

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const kmtariff = 0.21
 // let subtotal = kilometers / kmtariff;
 let total = kilometers / kmtariff;
 if (age < 18) {
-    // let total = ((total / 100) * 80).toFixed(2);
-    let total = `(${total} / 100) * 80`.toFixed(2);
+    let total = ((total / 100) * 80).toFixed(2);
+    // let total = `(${total} / 100) * 80`.toFixed(2);
 
 } else {
-    let total = `(${total} / 100) * 60`.toFixed(2);
+    // let total = `(${total} / 100) * 60`.toFixed(2);
+    let total = ((total / 100) * 60).toFixed(2);
     
 }
