c584e791ac677ad72bbfa8d5c6f6f155c2bb9920
DarioGuastella <jimmyguastella@gmail.com>
2023-10-26 17:51:34 +0200

    aggiunge sconti per età

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const kilometers  = prompt("Quanti chilometri vuoi percorrere?");
 const age = prompt ("Quanti anni hai?");
 const kmtariff = 0.21
-// let subtotal = kilometers / kmtariff;
 let total = kilometers * kmtariff;
 if (age < 18) {
     total = ((total / 100) * 80).toFixed(2);
-} else {
+} else if (age >=65) {
     total = ((total / 100) * 60).toFixed(2);    
 }
