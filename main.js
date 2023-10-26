f79351ea6ffa22c4e8393fdcdd0a53acff8a3e6d
DarioGuastella <jimmyguastella@gmail.com>
2023-10-26 17:25:38 +0200

    continua esercizio

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const kilometers  = prompt("Quanti chilometri vuoi percorrere?");
 const age = prompt ("Quanti anni hai?");
+const kmtariff = 0.21
+// let subtotal = kilometers / kmtariff;
+let total = kilometers / kmtariff;
+if (age < 18) {
+    // let total = ((total / 100) * 80).toFixed(2);
+    let total = `(${total} / 100) * 80`.toFixed(2);
+
+} else {
+    let total = `(${total} / 100) * 60`.toFixed(2);
+    
+}
