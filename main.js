0c5f28b928a47a074faf88bbf9097b6ba654201b
DarioGuastella <jimmyguastella@gmail.com>
2023-10-26 18:04:11 +0200

    aggiunge testi in pagina completa esercizio

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const kmtariff = 0.21
 let total = kilometers * kmtariff;
 if (age < 18) {
     total = ((total / 100) * 80).toFixed(2);
-} else if (age >=65) {
+} else if (age >= 65) {
     total = ((total / 100) * 60).toFixed(2);    
 }
+
+document.getElementById("title").innerHTML = `Il prezzo del biglietto è di:`;
+document.getElementById("price").innerHTML = `${total} €`;
+document.getElementById("price").style.color = "green";
